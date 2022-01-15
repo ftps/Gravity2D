806f2849e2af3c3d1e05c7d3d8fc1af43ea6376e
Jorge <jorgefilipecr@sapo.pt>
2022-01-15 20:09:21 +0000

    coomit

--- Headers/body.hpp
+++ Headers/body.hpp
@@ -1,8 +1,33 @@
 #ifndef BODY_HPP
 #define BODY_HPP
 
-class Body{
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class body{
+
+
+private:
+
+    // Physics variables
+    double mass;
+    double radius;
     
+
+    // Drawing variables
+    sf::CircleShape bodyShape; 
+
+
+
+
+
+
+
+public:
+
+    body();
+
 };
+
 
 #endif
--- Sources/body.cpp
+++ Sources/body.cpp
@@ -1,1 +1,15 @@
 #include "body.hpp"
+
+
+
+body::body()
+{
+
+    // Random color of body
+    sf::Color color(rand() % 255 + 1,rand() % 255 + 1,rand() % 255 + 1, 255);
+    bodyShape.setFillColor(color);
+
+
+
+
+}
